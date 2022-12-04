100a46676be6561a204815afe8c50ff34ea02786
mymorkkis <mvsy@protonmail.com>
2022-12-04 17:45:01 +0000

    Return http errors for the RatingsByUser unhappy paths
    Update the QueryRows parser function to take a Rows interface
    so that it's not coupled to pxgpool and can be mocker later.

--- db/db.go
+++ db/db.go
@@ -6,35 +6,44 @@ import (
 	"log"
 	"os"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-var databaseURL = fmt.Sprintf(
-	"postgres://%s:%s@db:5432/%s",
-	os.Getenv("POSTGRES_USER"),
-	os.Getenv("POSTGRES_PASSWORD"),
-	os.Getenv("POSTGRES_DB"),
-)
-
-func connect() *pgxpool.Pool {
-	dbpool, err := pgxpool.New(context.Background(), databaseURL)
-
-	if err != nil {
-		log.Fatalf("Unable to connect to database: %v\n", err)
-	}
-
-	return dbpool
+type Rows interface {
+	Next() bool
+	Scan(...any) error
 }
 
-func QueryRows[T Model](query string, parser func(pgx.Rows) []T, args ...any) []T {
-	dbpool := connect()
+func QueryRows[T Model](query string, parser func(Rows) ([]T, error), args ...any) ([]T, error) {
+	dbpool, err := connect()
+	if err != nil {
+		return nil, err
+	}
 	defer dbpool.Close()
 
 	rows, err := dbpool.Query(context.Background(), query, args...)
 	if err != nil {
 		log.Fatalf("Unable to execute ratings query: %v", err)
+		return nil, err
 	}
 
 	return parser(rows)
+}
+
+func connect() (*pgxpool.Pool, error) {
+	connectionURL := fmt.Sprintf(
+		"postgres://%s:%s@db:5432/%s",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+	)
+
+	dbpool, err := pgxpool.New(context.Background(), connectionURL)
+
+	if err != nil {
+		log.Printf("Unable to connect to database: %v\n", err)
+		return nil, err
+	}
+
+	return dbpool, nil
 }
--- handlers/ratings_by_user.go
+++ handlers/ratings_by_user.go
@@ -6,29 +6,32 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/jackc/pgx/v5"
 	"github.com/mymorkkis/brewdog-beer-rater-api/db"
 )
 
 func RatingsByUser(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
-	ratings := db.QueryRows(
+	ratings, err := db.QueryRows(
 		"select beer_id, rating from beer_ratings where user_id = $1;",
 		parseRows,
 		vars["userID"],
 	)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 
-	err := json.NewEncoder(w).Encode(&ratings)
+	err = json.NewEncoder(w).Encode(&ratings)
 	if err != nil {
-		log.Fatalf("Unable to encode queried data into JSON %v", err)
+		log.Printf("Unable to encode queried data into JSON %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
-
 }
 
-func parseRows(rows pgx.Rows) []db.BeerRating {
+func parseRows(rows db.Rows) ([]db.BeerRating, error) {
 	ratings := []db.BeerRating{}
 
 	for rows.Next() {
@@ -36,11 +39,12 @@ func parseRows(rows pgx.Rows) []db.BeerRating {
 
 		err := rows.Scan(&br.BeerID, &br.Rating)
 		if err != nil {
-			log.Fatalf("Unable to pass ratings row %v", err)
+			log.Printf("Unable to parse ratings by user row %v", err)
+			return nil, err
 		}
 
 		ratings = append(ratings, br)
 	}
 
-	return ratings
+	return ratings, nil
 }
